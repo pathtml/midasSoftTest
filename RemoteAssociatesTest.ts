dc715009b68ceff5faf987eab5a083d1cf604d1e
pathtml <pathtml@hotmail>
2023-08-24 14:09:25 +0700

    Q3 fixed

--- RemoteAssociatesTest.ts
+++ RemoteAssociatesTest.ts
@@ -2,45 +2,44 @@ function getQuestPart(phases: string[]): string[] {
     const set1 = phases
     let temp: string = ''
     let temp2: string = ''
-    let index:number = 0
+    let index: number = 0
     for (let i = 0; i < set1.length; i++) {
 
         let choice = set1[i].split('')
         let rest = set1.filter(item => set1[i] !== item)
 
-        for (let j = 0; j < rest.length; j++) {
-            for (let k = 0; k < choice.length; k++) {
+        for (let k = 0; k < choice.length; k++) {
 
-                if (temp2 === '' && rest[j].includes(choice[k])) {
-                        temp2 += choice[k]
-                        index++
-                } else if (rest[j].includes(temp2 + choice[k])) {
-                    temp2 += choice[k]
-                    index++
-                    // console.log(temp2);
-                } 
-                else if (rest[j].includes(choice[k])) {
-                    if (index>1) {
-                        temp = temp2
-                    }else{
-                        temp2 = choice[k]
-                    }
-                    // console.log(temp2);
-                } 
-
+            if (temp2 === '' && rest.every(item => item.includes(choice[k]))) {
+                temp2 += choice[k]
+                index++
+            } else if (rest.every(item => item.includes(temp2 + choice[k]))) {
+                temp2 += choice[k]
+                index++
+                // console.log(temp2);
+            }
+            else if (rest.every(item => item.includes(choice[k]))) {
+                if (index > 1) {
+                    temp = temp2
+                } else {
+                    temp2 = choice[k]
+                }
+                // console.log(temp2);
             }
 
         }
+
+
 
     }
     console.log(temp);
 
 
 
-    return phases.map(item=>item.replace(temp,''))
+    return phases.map(item => item.replace(temp, ''))
 }
 
-console.log(getQuestPart(['BEFRIEND', 'GIRLFRIEND', 'FRIENDSHIP']));
-console.log(getQuestPart(['BATHROOM', 'BATHSALT', 'BLOODBATH']));
+console.log(getQuestPart(['BEFRIEND', 'GIRLFRIEND', 'FRIENDSHIP', 'BEST FRIEND']));
+console.log(getQuestPart(['BATHROOM', 'BATHSALT', 'BLOODBATH', 'BATHTUB']));
 
 
